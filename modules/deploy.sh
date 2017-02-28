9a3cf29eacde4c01b8369767efb3b91e1dd28ac6
lbroudoux <laurent.broudoux@gmail.com>
2017-02-28 18:20:59 +0100

    Add accelerator stuffs

---
+++ modules/deploy.sh
@@ -0,0 +1,12 @@
+cd event-bus
+mvn -Pf8-local-deploy
+cd ../laboratoire
+mvn -Pf8-local-deploy
+cd ../imagerie
+mvn -Pf8-local-deploy
+cd ../uds-chirabdo
+mvn -Pf8-local-deploy
+cd ../administration
+mvn -Pf8-local-deploy
+cd ../api-gateway
+mvn -Pf8-local-deploy
